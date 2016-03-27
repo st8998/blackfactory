ccfead0b4abbc6f71a4444a12560af1ed8010d11
Ivan Efremov <st8998@gmail.com>
2016-03-27 12:03:19 +0400

    team page wip

--- src/users/team_page.js
+++ src/users/team_page.js
@@ -1,12 +1,20 @@
 import tmpl from './team_page_tmpl.slim'
+import * as userActions from 'users/users_actions'
 
 export default function register() {
-  return this.directive('teamPage', /* @ngInject */ function () {
+  return this.directive('teamPage', /* @ngInject */ function (Store) {
     return {
       restrict: 'E',
       template: tmpl,
       link: function (scope, el, attrs) {
         scope.page.currentPage = 'team'
+
+        Store.dispatch(userActions.requestAll())
+
+        Store.subscribe(function () {
+          scope.users = Store.getState().users.present
+        })
+
       },
     }
   })
